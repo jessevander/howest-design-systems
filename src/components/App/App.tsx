7939bf29e305fb1b979155ab6e31affe42798d47
Jesse Vandermeersch <jesse.vandermeersch@mateco.eu>
2024-11-24 13:41:28 +0100

    Themes solution

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,22 @@
+import {useState } from 'react';
 import AccessPage from '../Pages/AccessPage/AccessPage';
+import { Theme } from '../../types/Theme';
+import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState<Theme>('business');
+
+  const handleThemeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setTheme(e.target.value as Theme);
+    localStorage.setItem('theme', e.target.value);
+  }
+
   return (
-    <div className="app-wrapper">
+    <div className={`app-wrapper theme-${theme}`}>
+      <select id="theme-switcher" onChange={handleThemeChange }>
+        <option value='business'>Business</option>
+        <option value='education'>Education</option>
+      </select>
       <AccessPage />
     </div>
   );
--- src/components/elements/AccessForm/AccessForm.tsx
+++ src/components/elements/AccessForm/AccessForm.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react';
 import Button from '../../basics/Button/Button';
 import Input from '../../basics/FormElements/Input/Input';
 import './AccessForm.css';
 
 const AccessForm = () => {
+  const [title, setTitle] = useState<string>('Boost Productivity with VisionAR!');
+  const [subtitle, setSubtitle] = useState<string>('Empower your workforce with AR-driven efficiency and accuracy.');
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'education') {
+      setTitle('Reimagine Learning with VisionAR!');
+      setSubtitle('Transform classrooms into immersive spaces for hands-on learning.');
+    } else {
+      setTitle('Boost Productivity with VisionAR!');
+      setSubtitle('Empower your workforce with AR-driven efficiency and accuracy.');
+    }
+  }, [localStorage.getItem('theme')]);
+
   return (
     <div className='form-wrapper'>
       <div className='form-header'>
-        <h1>Boost Productivity with VisionAR!</h1>
-        <h2>Empower your workforce with AR-driven efficiency and accuracy.</h2>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
       </div>
       <div className='form-content'>
         <form>

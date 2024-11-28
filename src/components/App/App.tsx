5a25820fd12ee54d9871ceff180b6aad4e55804d
Jesse Vandermeersch <jesse.vandermeersch@mateco.eu>
2024-11-28 17:08:10 +0100

    update themes setup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
 import AccessPage from '../Pages/AccessPage/AccessPage';
 
+import './App.css';
+import { Theme } from '../../types/Theme';
+
 function App() {
+  const [theme, setTheme] = useState<Theme>('business');
+
+  const handleThemeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setTheme(e.target.value as Theme);
+    localStorage.setItem('theme', e.target.value);
+  }
+
   return (
-    <div className="app-wrapper">
+    <div className={`app-wrapper theme-${theme}`}>
+      <select id="theme-switcher" onChange={handleThemeChange }>
+        <option value='business'>Business</option>
+        <option value='education'>Education</option>
+      </select>
       <AccessPage />
     </div>
   );
